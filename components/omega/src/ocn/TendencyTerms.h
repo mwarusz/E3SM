718bb81be3ad974ba46c625b3b4e5cf339904de2
Maciej Waruszewski <mwarusz@igf.fuw.edu.pl>
2024-08-07 22:32:10 -0600

    Compute aux variables and tendencies in full halo for now

--- components/omega/src/ocn/TendencyTerms.h
+++ components/omega/src/ocn/TendencyTerms.h
@@ -365,9 +365,9 @@ class Tendencies {
    Tendencies(Tendencies &&)      = delete;
 
    // Mesh sizes
-   I4 NCellsOwned; ///< Number of cells owned by this task
-   I4 NEdgesOwned; ///< Number of edges owned by this task
-   I4 NChunks;     ///< Number of vertical level chunks
+   I4 NCellsAll; ///< Number of cells including full halo
+   I4 NEdgesAll; ///< Number of edges including full halo
+   I4 NChunks;   ///< Number of vertical level chunks
 
    // Pointer to default tendencies
    static Tendencies *DefaultTendencies;
