a96b1de71d4d915871cbdb7e456f20acf5032460
Maciej Waruszewski <mwarusz@igf.fuw.edu.pl>
2024-09-17 16:50:47 -0600

    Work

---
+++ components/omega/machines/cime/pm-gpu-gnugpu.cmake
@@ -0,0 +1,7 @@
+set(OMEGA_CIME_MACHINE pm-gpu CACHE STRING "")
+set(OMEGA_CIME_COMPILER gnugpu CACHE STRING "")
+set(OMEGA_BUILD_TYPE Release CACHE STRING "")
+set(OMEGA_BUILD_TEST ON CACHE BOOL "")
+
+set(OMEGA_PARMETIS_ROOT /global/cfs/cdirs/e3sm/software/polaris/pm-gpu/spack/dev_polaris_0_9_0_gnugpu_mpich/var/spack/environments/dev_polaris_0_9_0_gnugpu_mpich/.spack-env/view CACHE STRING "")
+set(OMEGA_MPI_ON_DEVICE OFF CACHE BOOL "")
