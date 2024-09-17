f546bd08ea3189ce4ddfa5dd9b64611d2b2e23f1
Maciej Waruszewski <mwarusz@igf.fuw.edu.pl>
2024-09-17 16:50:47 -0600

    Work

---
+++ components/omega/machines/cime/frontier-craygnu-mphipcc.cmake
@@ -0,0 +1,8 @@
+set(OMEGA_CIME_MACHINE frontier CACHE STRING "")
+set(OMEGA_CIME_COMPILER craygnu-mphipcc CACHE STRING "")
+set(OMEGA_BUILD_TYPE Release CACHE STRING "")
+set(OMEGA_BUILD_TEST ON CACHE BOOL "")
+set(OMEGA_PARMETIS_ROOT /ccs/proj/cli115/software/polaris/frontier/spack/dev_polaris_0_9_0_craygnu-mphipcc_mpich/var/spack/environments/dev_polaris_0_9_0_craygnu-mphipcc_mpich/.spack-env/view CACHE STRING "")
+
+
+set(OMEGA_VECTOR_LENGTH 1 CACHE STRING "")
