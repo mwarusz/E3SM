d07964686d52ead6ddbaeaa285aa7b153153223e
Maciej Waruszewski <mwarusz@igf.fuw.edu.pl>
2024-09-17 16:50:47 -0600

    Work

---
+++ components/omega/machines/nocime/workmac-clang.cmake
@@ -0,0 +1,21 @@
+set(OMEGA_SKIP_CIME TRUE CACHE BOOL "")
+
+set(OMEGA_C_COMPILER "mpicc" CACHE STRING "")
+set(OMEGA_CXX_COMPILER "mpicxx" CACHE STRING "")
+set(OMEGA_Fortran_COMPILER "mpifort" CACHE STRING "")
+
+set(OMEGA_ARCH SERIAL CACHE STRING "")
+set(Kokkos_ARCH_NATIVE ON CACHE BOOL "")
+
+set(Kokkos_ENABLE_DEBUG ON CACHE BOOL "")
+set(Kokkos_ENABLE_DEBUG_BOUNDS_CHECK ON CACHE BOOL "")
+
+set(OMEGA_GKLIB_ROOT "/Users/mwarusz/installs/gklib" CACHE STRING "")
+set(OMEGA_METIS_ROOT "/Users/mwarusz/installs/metis" CACHE STRING "")
+set(OMEGA_PARMETIS_ROOT "/Users/mwarusz/installs/parmetis" CACHE STRING "")
+
+set(CMAKE_C_FLAGS -g CACHE STRING "")
+set(CMAKE_CXX_FLAGS -g CACHE STRING "")
+
+set(OMEGA_MPI_EXEC mpirun CACHE STRING "")
+set(OMEGA_BUILD_TEST ON CACHE BOOL "")
