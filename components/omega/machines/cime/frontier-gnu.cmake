a60e4fec61fa0fcd417175df1102d66ab6ee28a5
Maciej Waruszewski <mwarusz@igf.fuw.edu.pl>
2024-09-17 16:50:47 -0600

    Work

---
+++ components/omega/machines/cime/frontier-gnu.cmake
@@ -0,0 +1,7 @@
+set(OMEGA_CIME_MACHINE frontier CACHE STRING "")
+set(OMEGA_CIME_COMPILER gnu CACHE STRING "")
+set(OMEGA_BUILD_TYPE Release CACHE STRING "")
+set(OMEGA_BUILD_TEST ON CACHE BOOL "")
+set(OMEGA_PARMETIS_ROOT /ccs/proj/cli115/software/polaris/frontier/spack/dev_polaris_0_5_0_gnu_mpich/var/spack/environments/dev_polaris_0_5_0_gnu_mpich/.spack-env/view CACHE STRING "")
+
+set(OMEGA_VECTOR_LENGTH 4 CACHE STRING "")
