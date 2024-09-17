7e827ce2f4f4ebc71cf2e0f3a25203ace345bdce
Maciej Waruszewski <mwarusz@igf.fuw.edu.pl>
2024-09-17 16:50:47 -0600

    Work

---
+++ components/omega/machines/cime/aurora-ifx.cmake
@@ -0,0 +1,7 @@
+set(OMEGA_CIME_MACHINE aurora CACHE STRING "")
+set(OMEGA_CIME_COMPILER oneapi-ifx CACHE STRING "")
+set(OMEGA_BUILD_TYPE Release CACHE STRING "")
+set(OMEGA_BUILD_TEST ON CACHE BOOL "")
+set(OMEGA_PARMETIS_ROOT /lus/flare/projects/E3SM_Dec/soft/polaris/aurora/spack/dev_polaris_0_8_0_oneapi-ifx_mpich/var/spack/environments/dev_polaris_0_8_0_oneapi-ifx_mpich/.spack-env/view CACHE STRING "")
+
+set(OMEGA_VECTOR_LENGTH 1 CACHE STRING "")
