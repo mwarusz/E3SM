abe2ceb53dcd20be0c61dcf0354e98f93053b7c8
Maciej Waruszewski <mwarusz@igf.fuw.edu.pl>
2024-09-17 16:50:47 -0600

    Work

---
+++ components/omega/machines/nocime/liczydlo-gpu-amdclang.cmake
@@ -0,0 +1,18 @@
+set(OMEGA_SKIP_CIME TRUE CACHE BOOL "")
+
+set(OMEGA_C_COMPILER "$ENV{HOME}/installs/openmpi-amdclang/bin/mpicc" CACHE STRING "")
+set(OMEGA_CXX_COMPILER "$ENV{HOME}/installs/openmpi-amdclang/bin/mpicxx" CACHE STRING "")
+set(OMEGA_Fortran_COMPILER "$ENV{HOME}/installs/openmpi-amdclang/bin/mpifort" CACHE STRING "")
+
+set(OMEGA_ARCH HIP CACHE STRING "")
+set(Kokkos_ARCH_NATIVE ON CACHE BOOL "")
+set(Kokkos_ARCH_GFX1103 ON CACHE BOOL "")
+
+set(MPILIB_NAME "openmpi" CACHE STRING "")
+set(OMEGA_MPI_EXEC "$ENV{HOME}/installs/openmpi-amdclang/bin/mpirun" CACHE STRING "")
+
+set(OMEGA_GKLIB_ROOT "/usr/" CACHE STRING "")
+set(OMEGA_METIS_ROOT "/usr/" CACHE STRING "")
+set(OMEGA_PARMETIS_ROOT "/usr/" CACHE STRING "")
+
+set(OMEGA_BUILD_TEST ON CACHE BOOL "")
