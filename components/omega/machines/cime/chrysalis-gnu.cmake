4e2b9d8ee465570ac9a47ed63e48633545b0a92c
Maciej Waruszewski <mwarusz@igf.fuw.edu.pl>
2024-09-17 16:50:47 -0600

    Work

---
+++ components/omega/machines/cime/chrysalis-gnu.cmake
@@ -0,0 +1,7 @@
+set(OMEGA_CIME_MACHINE chrysalis CACHE STRING "")
+set(OMEGA_CIME_COMPILER gnu CACHE STRING "")
+set(OMEGA_BUILD_TYPE Release CACHE STRING "")
+set(OMEGA_BUILD_TEST ON CACHE BOOL "")
+set(OMEGA_PARMETIS_ROOT /lcrc/soft/climate/polaris/chrysalis/spack/dev_polaris_0_5_0_gnu_openmpi/var/spack/environments/dev_polaris_0_5_0_gnu_openmpi/.spack-env/view CACHE STRING "")
+
+set(OMEGA_VECTOR_LENGTH 4 CACHE STRING "")
---
+++ components/omega/machines/cime/pm-cpu-nvidia.cmake
@@ -0,0 +1,7 @@
+set(OMEGA_CIME_MACHINE pm-cpu CACHE STRING "")
+set(OMEGA_CIME_COMPILER nvidia CACHE STRING "")
+set(OMEGA_BUILD_TYPE Release CACHE STRING "")
+set(OMEGA_BUILD_TEST ON CACHE BOOL "")
+set(OMEGA_PARMETIS_ROOT /global/cfs/cdirs/e3sm/software/polaris/pm-cpu/spack/dev_polaris_0_5_0_nvidia_mpich/var/spack/environments/dev_polaris_0_5_0_nvidia_mpich/.spack-env/view CACHE STRING "")
+
+set(OMEGA_VECTOR_LENGTH 4 CACHE STRING "")
