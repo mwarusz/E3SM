5d34f47b6f2a4fbff08ff7379d2eda218eb269d5
Maciej Waruszewski <mwarusz@igf.fuw.edu.pl>
2024-09-23 17:52:56 -0400

    frontier machine files

--- components/omega/machines/frontier-gpu-amdclang.cmake
+++ components/omega/machines/frontier-gpu-amdclang.cmake
@@ -4,12 +4,14 @@ set(OMEGA_CXX_COMPILER "mpicxx" CACHE STRING "")
 set(OMEGA_Fortran_COMPILER "mpifort" CACHE STRING "")
 
 set(OMEGA_CXX_FLAGS "-g" CACHE STRING "")
-set(OMEGA_ARCH SERIAL CACHE STRING "")
+set(MPILIB_NAME mpich CACHE STRING "")
+set(OMEGA_ARCH HIP CACHE STRING "")
 set(Kokkos_ARCH_NATIVE ON CACHE BOOL "")
+set(Kokkos_ARCH_GFX90A ON CACHE BOOL "")
 
 set(OMEGA_PARMETIS_ROOT /ccs/proj/cli115/software/polaris/frontier/spack/dev_polaris_0_4_0_gnu_mpich/var/spack/environments/dev_polaris_0_4_0_gnu_mpich/.spack-env/view CACHE STRING "")
 
-set(OMEGA_VECTOR_LENGTH 4 CACHE STRING "")
+set(OMEGA_VECTOR_LENGTH 1 CACHE STRING "")
 set(OMEGA_FUSED_VELTEND ON CACHE BOOL "")
 set(OMEGA_FUSED_ACCUM ON CACHE BOOL "")
 
