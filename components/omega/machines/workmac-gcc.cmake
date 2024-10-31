1527d3442eafc4af3d4f12a0566f43e32322da43
Maciej Waruszewski <mwarusz@igf.fuw.edu.pl>
2024-10-31 11:56:16 -0600

    Update workmac-gcc

--- components/omega/machines/workmac-gcc.cmake
+++ components/omega/machines/workmac-gcc.cmake
@@ -3,13 +3,20 @@ set(OMEGA_C_COMPILER "mpicc" CACHE STRING "")
 set(OMEGA_CXX_COMPILER "mpicxx" CACHE STRING "")
 set(OMEGA_Fortran_COMPILER "mpifort" CACHE STRING "")
 
+set(OMEGA_CXX_FLAGS "-g -march=native" CACHE STRING "")
 set(OMEGA_ARCH SERIAL CACHE STRING "")
+#set(Kokkos_ARCH_NATIVE ON CACHE BOOL "")
 
 set(OMEGA_GKLIB_ROOT "/Users/mwarusz/omega/gklib" CACHE STRING "")
 set(OMEGA_METIS_ROOT "/Users/mwarusz/omega/metis" CACHE STRING "")
 set(OMEGA_PARMETIS_ROOT "/Users/mwarusz/omega/parmetis" CACHE STRING "")
 
-set(OMEGA_CXX_FLAGS -march=native CACHE STRING "")
+set(OMEGA_VECTOR_LENGTH 2 CACHE STRING "")
+set(OMEGA_FUSED_VELTEND OFF CACHE BOOL "")
+set(OMEGA_FUSED_ACCUM OFF CACHE BOOL "")
 
-set(OMEGA_MPI_EXEC mpirun CACHE STRING "")
+set(OMEGA_MPI_EXEC srun CACHE STRING "")
 set(OMEGA_BUILD_TEST ON CACHE BOOL "")
+
+set(OMEGA_USE_CALIPER ON CACHE BOOL "")
+set(caliper_DIR $ENV{HOME}/installs/caliper/share/cmake/caliper CACHE STRING "")
