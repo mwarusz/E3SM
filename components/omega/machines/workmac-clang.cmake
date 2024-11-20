29ff22f58e6137cc179d26219ae55b499b988e5c
Maciej Waruszewski <mwarusz@igf.fuw.edu.pl>
2024-11-20 08:46:05 -0700

    workmac-gcc > workmac-clang

--- components/omega/machines/workmac-clang.cmake
+++ components/omega/machines/workmac-clang.cmake
@@ -4,12 +4,13 @@ set(OMEGA_CXX_COMPILER "mpicxx" CACHE STRING "")
 set(OMEGA_Fortran_COMPILER "mpifort" CACHE STRING "")
 
 set(OMEGA_ARCH SERIAL CACHE STRING "")
+set(Kokkos_ARCH_NATIVE ON CACHE BOOL "")
 
-set(OMEGA_GKLIB_ROOT "/Users/mwarusz/omega/gklib" CACHE STRING "")
-set(OMEGA_METIS_ROOT "/Users/mwarusz/omega/metis" CACHE STRING "")
-set(OMEGA_PARMETIS_ROOT "/Users/mwarusz/omega/parmetis" CACHE STRING "")
+set(OMEGA_GKLIB_ROOT "/Users/mwarusz/installs/gklib" CACHE STRING "")
+set(OMEGA_METIS_ROOT "/Users/mwarusz/installs/metis" CACHE STRING "")
+set(OMEGA_PARMETIS_ROOT "/Users/mwarusz/installs/parmetis" CACHE STRING "")
 
-set(OMEGA_CXX_FLAGS -march=native CACHE STRING "")
+set(OMEGA_CXX_FLAGS -g CACHE STRING "")
 
 set(OMEGA_MPI_EXEC mpirun CACHE STRING "")
 set(OMEGA_BUILD_TEST ON CACHE BOOL "")
