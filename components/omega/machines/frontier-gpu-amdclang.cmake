a694f6d242e29c3425a2b74b28c732eb4ce2adfb
Maciej Waruszewski <mwarusz@igf.fuw.edu.pl>
2024-11-21 16:19:58 -0700

    Add a ton of machine files

---
+++ components/omega/machines/frontier-gpu-amdclang.cmake
@@ -0,0 +1,23 @@
+set(OMEGA_SKIP_CIME TRUE CACHE BOOL "")
+set(OMEGA_C_COMPILER "mpicc" CACHE STRING "")
+set(OMEGA_CXX_COMPILER "mpicxx" CACHE STRING "")
+set(OMEGA_Fortran_COMPILER "mpifort" CACHE STRING "")
+
+set(OMEGA_CXX_FLAGS "-g" CACHE STRING "")
+set(MPILIB_NAME mpich CACHE STRING "")
+set(OMEGA_ARCH HIP CACHE STRING "")
+set(Kokkos_ARCH_NATIVE ON CACHE BOOL "")
+set(Kokkos_ARCH_GFX90A ON CACHE BOOL "")
+
+set(OMEGA_PARMETIS_ROOT /ccs/proj/cli115/software/polaris/frontier/spack/dev_polaris_0_4_0_gnu_mpich/var/spack/environments/dev_polaris_0_4_0_gnu_mpich/.spack-env/view CACHE STRING "")
+
+set(OMEGA_VECTOR_LENGTH 1 CACHE STRING "")
+
+set(OMEGA_MPI_EXEC srun CACHE STRING "")
+set(OMEGA_BUILD_TEST ON CACHE BOOL "")
+
+set(NetCDF_PATH $ENV{NETCDF_DIR} CACHE STRING "")
+set(PnetCDF_PATH $ENV{NETCDF_DIR} CACHE STRING "")
+
+set(OMEGA_USE_CALIPER ON CACHE BOOL "")
+set(caliper_DIR $ENV{HOME}/installs/caliper/share/cmake/caliper CACHE STRING "")
---
+++ components/omega/machines/perlmutter-gpu-gcc.cmake
@@ -0,0 +1,23 @@
+set(OMEGA_SKIP_CIME TRUE CACHE BOOL "")
+set(OMEGA_C_COMPILER "mpicc" CACHE STRING "")
+set(OMEGA_CXX_COMPILER "mpicxx" CACHE STRING "")
+set(OMEGA_Fortran_COMPILER "mpifort" CACHE STRING "")
+
+set(OMEGA_CXX_FLAGS "-g" CACHE STRING "")
+set(OMEGA_ARCH CUDA CACHE STRING "")
+set(Kokkos_ARCH_NATIVE ON CACHE BOOL "")
+set(Kokkos_ARCH_AMPERE80 ON CACHE BOOL "")
+set(Kokkos_ENABLE_CUDA_CONSTEXPR ON CACHE BOOL "")
+
+set(OMEGA_PARMETIS_ROOT /global/cfs/cdirs/e3sm/software/polaris/pm-cpu/spack/dev_polaris_0_4_0_gnu_mpich/var/spack/environments/dev_polaris_0_4_0_gnu_mpich/.spack-env/view CACHE STRING "")
+
+set(OMEGA_VECTOR_LENGTH 1 CACHE STRING "")
+
+set(OMEGA_MPI_EXEC srun CACHE STRING "")
+set(OMEGA_BUILD_TEST ON CACHE BOOL "")
+
+set(NetCDF_PATH $ENV{CRAY_NETCDF_HDF5PARALLEL_PREFIX} CACHE STRING "")
+set(PnetCDF_PATH $ENV{CRAY_PARALLEL_NETCDF_PREFIX} CACHE STRING "")
+
+set(OMEGA_USE_CALIPER ON CACHE BOOL "")
+set(caliper_DIR $ENV{HOME}/installs/caliper/share/cmake/caliper CACHE STRING "")
