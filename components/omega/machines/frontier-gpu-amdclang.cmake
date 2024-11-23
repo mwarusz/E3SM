92a0c785efd1c667bce43d7b539b0a3c3dd8ee11
Maciej Waruszewski <mwarusz@igf.fuw.edu.pl>
2024-11-22 19:11:49 -0500

    Update frontier for gpu aware mpi

--- components/omega/machines/frontier-gpu-amdclang.cmake
+++ components/omega/machines/frontier-gpu-amdclang.cmake
@@ -12,8 +12,8 @@ set(Kokkos_ARCH_GFX90A ON CACHE BOOL "")
 set(OMEGA_PARMETIS_ROOT /ccs/proj/cli115/software/polaris/frontier/spack/dev_polaris_0_4_0_gnu_mpich/var/spack/environments/dev_polaris_0_4_0_gnu_mpich/.spack-env/view CACHE STRING "")
 
 set(OMEGA_VECTOR_LENGTH 1 CACHE STRING "")
-
-set(OMEGA_MPI_ON_DEVICE OFF CACHE BOOL "")
+set(OMEGA_MPI_ON_DEVICE ON CACHE BOOL "")
+set(OMEGA_LINK_OPTIONS -L$ENV{MPICH_DIR}/lib -lmpi $ENV{CRAY_XPMEM_POST_LINK_OPTS} -lxpmem $ENV{PE_MPICH_GTL_DIR_amd_gfx90a} $ENV{PE_MPICH_GTL_LIBS_amd_gfx90a} CACHE STRING "")
 
 set(OMEGA_MPI_EXEC srun CACHE STRING "")
 set(OMEGA_BUILD_TEST ON CACHE BOOL "")
