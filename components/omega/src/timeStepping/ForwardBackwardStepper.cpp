4e8339cf043934d2aa8ab1b610888d3b10cac29d
Maciej Waruszewski <mwarusz@igf.fuw.edu.pl>
2025-04-21 20:17:10 -0600

    Comment out halo timing

--- components/omega/src/timeStepping/ForwardBackwardStepper.cpp
+++ components/omega/src/timeStepping/ForwardBackwardStepper.cpp
@@ -68,25 +68,25 @@ void ForwardBackwardStepper::doStep(
    // Update time levels (New -> Old) of prognostic variables with halo
    // exchanges
 
-   static int step = 0;
+   // static int step = 0;
 
-   if (step == 0) {
-      State->updateTimeLevels();
-      Tracers::updateTimeLevels();
-      step++;
-   } else {
-      const auto Comm = MachEnv::getDefault()->getComm();
-      MPI_Barrier(Comm);
-      Kokkos::fence();
-      Pacer::start("updateTimeLevels");
+   // if (step == 0) {
+   State->updateTimeLevels();
+   Tracers::updateTimeLevels();
+   //   step++;
+   //} else {
+   //   const auto Comm = MachEnv::getDefault()->getComm();
+   //   MPI_Barrier(Comm);
+   //   Kokkos::fence();
+   //   Pacer::start("updateTimeLevels");
 
-      State->updateTimeLevels();
-      Tracers::updateTimeLevels();
+   //   State->updateTimeLevels();
+   //   Tracers::updateTimeLevels();
 
-      Pacer::stop("updateTimeLevels");
-      Kokkos::fence();
-      MPI_Barrier(Comm);
-   }
+   //   Pacer::stop("updateTimeLevels");
+   //   Kokkos::fence();
+   //   MPI_Barrier(Comm);
+   //}
 
    // Advance the clock and update the simulation time
    Err     = StepClock->advance();
