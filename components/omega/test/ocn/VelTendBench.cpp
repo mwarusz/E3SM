0debde20459f53646d7b2a65b4ecc6f04b655326
Maciej Waruszewski <mwarusz@igf.fuw.edu.pl>
2026-05-15 16:08:59 -0600

    Separate TU

--- components/omega/test/ocn/VelTendBench.cpp
+++ components/omega/test/ocn/VelTendBench.cpp
@@ -50,66 +50,7 @@ void velTend(const Array2DReal &LocNormalVelocityTend,
              const Array2DReal &SSHCell, const Array2DReal &DivCell,
              const Array2DReal &RVortVertex, const Array2DReal &Del2DivCell,
              const Array2DReal &Del2RVortVertex, const HorzMesh *Mesh,
-             const VertCoord *VCoord) {
-
-   const int NVertLayers       = VCoord->NVertLayers;
-   const auto &MinLayerEdgeBot = VCoord->MinLayerEdgeBot;
-   const auto &MaxLayerEdgeTop = VCoord->MinLayerEdgeTop;
-
-   PotentialVortHAdvOnEdge LocPotentialVortHAdv(Mesh, VCoord);
-   KEGradOnEdge LocKEGrad(Mesh, VCoord);
-   SSHGradOnEdge LocSSHGrad(Mesh, VCoord);
-   VelocityDiffusionOnEdge LocVelocityDiffusion(Mesh, VCoord);
-   VelocityHyperDiffOnEdge LocVelocityHyperDiff(Mesh, VCoord);
-
-   LocPotentialVortHAdv.Enabled = true;
-   LocKEGrad.Enabled            = true;
-   LocSSHGrad.Enabled           = true;
-   LocVelocityDiffusion.Enabled = true;
-   LocVelocityHyperDiff.Enabled = true;
-
-   const auto LConfig = LaunchConfig({Mesh->NEdgesOwned}, NVertLayers,
-                                     TeamScratch<Real>(NVertLayers));
-   parallelForOuter(
-       LConfig, KOKKOS_LAMBDA(int IEdge, const TeamMember &Team) {
-          ArrayScratch1DReal VelTendScratch(teamScratch(Team), NVertLayers);
-
-          const int KMin = MinLayerEdgeBot(IEdge);
-          const int KMax = MaxLayerEdgeTop(IEdge);
-
-          parallelForInnerOpt(
-              Team, Range{KMin, KMax},
-              INNER_LAMBDA(int K) { VelTendScratch(K) = 0; });
-
-          if (LocPotentialVortHAdv.Enabled) {
-             LocPotentialVortHAdv(Team, VelTendScratch, IEdge, NormRVortEdge,
-                                  NormFEdge, FluxLayerThickEdge, NormVelEdge);
-          }
-
-          if (LocKEGrad.Enabled) {
-             LocKEGrad(Team, VelTendScratch, IEdge, KECell);
-          }
-
-          if (LocSSHGrad.Enabled) {
-             LocSSHGrad(Team, VelTendScratch, IEdge, SSHCell);
-          }
-
-          if (LocVelocityDiffusion.Enabled) {
-             LocVelocityDiffusion(Team, VelTendScratch, IEdge, DivCell,
-                                  RVortVertex);
-          }
-
-          if (LocVelocityHyperDiff.Enabled) {
-             LocVelocityHyperDiff(Team, VelTendScratch, IEdge, Del2DivCell,
-                                  Del2RVortVertex);
-          }
-
-          parallelForInnerOpt(
-              Team, Range{KMin, KMax}, INNER_LAMBDA(int K) {
-                 LocNormalVelocityTend(IEdge, K) = VelTendScratch(K);
-              });
-       });
-}
+             const VertCoord *VCoord);
 
 int benchVelTend(int NVertLayers) {
 
