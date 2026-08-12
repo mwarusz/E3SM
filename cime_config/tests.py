cd9e1e1d59f4d5de6b75f2a8fb32b4c47250a2fd
Andrew Nolan <anolan@lanl.gov>
2026-08-12 12:34:18 -0400

    Comment out PEM, add SMS tests in their stead

--- cime_config/tests.py
+++ cime_config/tests.py
@@ -1148,10 +1148,12 @@ _TESTS = {
         "time"  : "0:30:00",
         "share" : True,
         "tests" : (
+            "SMS_Vmct.T62_oQU240.COMEGA-IAF",
             "ERS_Vmct.T62_oQU240.COMEGA-IAF",
-            "PEM_Vmct.T62_oQU240.COMEGA-IAF",
+            #"PEM_Vmct.T62_oQU240.COMEGA-IAF",
+            "SMS_Vmct_Ln5.TL319_EC30to60E2r2.COMEGA-JRA1p5.omega-jra_1958",
             "ERS_Vmct_Ln5.TL319_EC30to60E2r2.COMEGA-JRA1p5.omega-jra_1958",
-            "PEM_Vmct_Ln5.TL319_EC30to60E2r2.COMEGA-JRA1p5.omega-jra_1958",
+            #"PEM_Vmct_Ln5.TL319_EC30to60E2r2.COMEGA-JRA1p5.omega-jra_1958",
             )
     },
 }
