b879387c0bda71ededebd591e430920fa7d53f7a
Andrew Nolan <32367657+andrewdnolan@users.noreply.github.com>
2026-08-12 09:09:51 -0600

    Apply suggestions from code review
    Co-authored-by: Hyun (Hyun-Gyu) Kang <47987430+hyungyukang@users.noreply.github.com>

--- components/omega/src/timeStepping/ForwardBackwardStepper.cpp
+++ components/omega/src/timeStepping/ForwardBackwardStepper.cpp
@@ -62,8 +62,6 @@ void ForwardBackwardStepper::doStep(
    // u^{n+1} = u^{n} + R_u^{n}
    updateVelocityByTend(State, VelNextLevel, State, VelCurLevel, TimeStep);
 
-   updateVelocityByTend(State, VelNextLevel, State, VelCurLevel, TimeStep);
-
    prescribeVelocity(State, VelNextLevel, State, VelCurLevel,
                      SimTime + TimeStep);
 
@@ -82,13 +80,6 @@ void ForwardBackwardStepper::doStep(
    updateThicknessByTend(State, ThickNextLevel, State, ThickCurLevel, TimeStep);
 
    prescribeThickness(State, ThickNextLevel, State, ThickCurLevel);
-
-   // Exchange halo of h^{n+1} before it is used below
-   Array2DReal NextPseudoThickness = State->getPseudoThickness(ThickNextLevel);
-   Pacer::timingBarrier("ForwardBackward:thickHaloExchBarrier", 3, Comm);
-   Pacer::start("ForwardBackward:thickHaloExch", 3);
-   MeshHalo->exchangeFullArrayHalo(NextPseudoThickness, OnCell);
-   Pacer::stop("ForwardBackward:thickHaloExch", 3);
 
    // R_phi^{n} = RHS_phi(u^{n+1}, h^{n+1}, phi^{n}, t^{n})
    Tend->computeTracerTendencies(State, AuxState, CurTracerArray,
