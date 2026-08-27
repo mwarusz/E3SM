96a0efa9f9c438318c693c3a5f732ddcf4970f43
Maciej Waruszewski <mwarusz@igf.fuw.edu.pl>
2026-08-27 14:12:11 -0600

    Allow transport velocity correction in computePseudoThicknessTracerAux

--- components/omega/src/ocn/AuxiliaryState.cpp
+++ components/omega/src/ocn/AuxiliaryState.cpp
@@ -410,7 +410,8 @@ void AuxiliaryState::computeAll(const OceanState *State,
 
 void AuxiliaryState::computePseudoThicknessTracerAux(
     const OceanState *State, const Array3DReal &TracerArray, int ThickTimeLevel,
-    int VelTimeLevel, const TimeInterval ProjDt) const {
+    int VelTimeLevel, const TimeInterval ProjDt,
+    const Array2DReal &TransportVelocityAdd) const {
 
    Array2DReal PseudoThickCell = State->getPseudoThickness(ThickTimeLevel);
    Array2DReal NormalVelEdge   = State->getNormalVelocity(VelTimeLevel);
